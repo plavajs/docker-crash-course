76eb2ff263e42d503facea493c5f7d8edb9ac9db
Lukáš Mlnářík <mlnarik.lukas@seznam.cz>
2022-07-13 17:34:46 +0200

    added course project

--- api/app.js
+++ api/app.js
@@ -8,20 +8,20 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.json([
     {
-      "id":"1",
-      "title":"Book Review: The Bear & The Nightingale"
+      "id": "1",
+      "title": "Book Review: The Bear & The Nightingale abrakadabra"
     },
     {
-      "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "id": "2",
+      "title": "Game Review: Pokemon Brillian Diamond...."
     },
     {
-      "id":"3",
-      "title":"Show Review: Alice in Borderland"
+      "id": "3",
+      "title": "Show Review: Alice in Borderland"
     }
   ])
 })
 
 app.listen(4000, () => {
-  console.log('listening for requests on port 4000')
+  console.log('listening for requests on port 4000......')
 })
